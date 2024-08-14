1f0e13619eb42679570f1d2206faac4d8100473b
Robin Cunningham <79066805+rspcunningham@users.noreply.github.com>
2024-08-13 21:03:59 -0400

    fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,10 +14,11 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://documentation.parasight.ca/',
+  url: 'https://rspcunningham.github.io/',
+  baseUrl: '/fpm-py-docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  //baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
